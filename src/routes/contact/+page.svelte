b99c3a549fe6e16efcbdb26e8ffc1349d535c5c5
Andrei <andrei.dragomir@ogooga.com>
2024-10-17 16:02:15 +0200

    contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import CasinoInfo from "../../lib/components/CasinoInfo.svelte";
+  import HeroBlockContact from "../../lib/components/Hero-Block-Contact.svelte";
+
+</script>
+
+<div class="container py-5">
+  <div class="mt-5">
+    <h1 class="text-center">Contact us</h1>
+  </div>
+
+  <CasinoInfo />
+  <HeroBlockContact />
+
+</div>
